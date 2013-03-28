5357fba64219275f728c2d345b85f024e66b3204
John Hatton <hattonjohn@gmail.com>
2013-03-28 09:21:09 -0700

    More name change and folder organization

---
+++ CopyTo Bloom.bat
@@ -0,0 +1,7 @@
+copy /Y output\debug\*.dll c:\dev\bloom\lib\dotnet
+copy /Y output\debug\*.xml c:\dev\bloom\lib\dotnet
+copy /Y output\debug\*.pdb c:\dev\bloom\lib\dotnet
+
+copy /Y c:\dev\bloom\lib\dotnet\Localization*.* c:\dev\bloom\output\debug
+
+pause
