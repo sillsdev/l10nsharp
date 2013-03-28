8f3e051280c81053fa273d6e671c2fecaf282eae
John Hatton <hattonjohn@gmail.com>
2013-03-28 10:05:53 -0700

    Remove a bunch of stuff that snuck in to the repository

--- CopyTo Saymore.bat
+++
@@ -1,7 +0,0 @@
-copy /Y output\debug\*.dll c:\dev\saymore\lib\dotnet
-copy /Y output\debug\*.xml c:\dev\saymore\lib\dotnet
-copy /Y output\debug\*.pdb c:\dev\saymore\lib\dotnet
-
-copy /Y c:\dev\saymore\lib\dotnet\Localization*.* c:\dev\saymore\output\debug
-
-pause
--- GetBuildCopy.bat
+++
@@ -1,6 +0,0 @@
-hg pull -u
-REM %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"" x86
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-msbuild "Localization.sln"
-call "copyto bloom".bat
-call "copyto saymore".bat
