3f9a7f3de0096e9c227d68c316b2a744ad78a25a
Stephen McConnel <stephen_mcconnel@sil.org>
2017-11-27 17:21:29 -0700

    Handle languages the system doesn't know about (BL-5260)
    We can't depend on CultureInfo working for every language that our
    programs may be localized into.  Mono 4 throws exceptions while
    .Net 4.6.1 creates generally worthless objects for languages that
    the authors didn't know about or didn't consider worth implementing.

---
+++ src/L10NSharpTests/L10NCultureInfoTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using L10NSharp.XLiffUtils;
+using L10NSharp.UI;
+using NUnit.Framework;
+
+namespace L10NSharp.Tests
+{
+	[TestFixture]
+	public class L10NCultureInfoTests
+	{
+		[Test]
+		public void L10NCultureInfo_TestEn()
+		{
+			var enci = L10NCultureInfo.GetCultureInfo("en");
+			Assert.AreEqual("English", enci.EnglishName);
+			Assert.IsNotNull(enci.RawCultureInfo);
+		}
+
+		[Test]
+		public void L10NCultureInfo_TestPbu()
+		{
+			var pbuci = L10NCultureInfo.GetCultureInfo("pbu");
+			Assert.AreEqual("Northern Pashto", pbuci.EnglishName);
+			Assert.IsNull(pbuci.RawCultureInfo);
+		}
+
+		[Test]
+		public void L10NCultureInfo_TestList()
+		{
+			var list = L10NCultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+			Assert.IsTrue(list.Contains(L10NCultureInfo.GetCultureInfo("en")));
+			Assert.IsTrue(list.Contains(L10NCultureInfo.GetCultureInfo("pbu")));
+			Assert.IsTrue(list.Contains(L10NCultureInfo.GetCultureInfo("prs")));
+		}
+	}
+}
