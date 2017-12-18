f704331422fd8cd5c8a4bd5397867e0a2cc94cfd
Stephen McConnel <stephen_mcconnel@sil.org>
2017-12-18 15:01:47 -0700

    Handle notes from old xliff during merge
    Also remove a couple of debug output statements

--- src/L10NSharpTests/LocalizationManagerTests.cs
+++ src/L10NSharpTests/LocalizationManagerTests.cs
@@ -649,7 +649,7 @@ namespace L10NSharp.Tests
 			CheckMergedTransUnit(tu, "That was a test.", new[] {"ID: That.test", "That is hard to explain, but a literal rendition is okay.", "Not found in static scan of compiled code (version 3.1.4)"}, false);
 
 			tu = mergedDoc.GetTransUnitForId("What.test");
-			CheckMergedTransUnit(tu, "What is a good test?", new[] {"ID: What.test", "OLD (before 3.1.4): What is good test."}, true);
+			CheckMergedTransUnit(tu, "What is a good test?", new[] {"ID: What.test", "[OLD NOTE] Whatever you say...", "OLD TEXT (before 3.1.4): What is good test."}, true);
 
 			tu = mergedDoc.GetTransUnitForId("What.this");
 			CheckMergedTransUnit(tu, "What is this nonsense?", new[] {"ID: What.this", "Not dynamic: found in static scan of compiled code (version 3.1.4)"}, false);
@@ -706,7 +706,7 @@ namespace L10NSharp.Tests
 			doc.AddTransUnit(tu1);
 			var tu2 = CreateTestTransUnit("That.test", "That was a test.", new[] {"That is hard to explain, but a literal rendition is okay."}, false);
 			doc.AddTransUnit(tu2);
-			var tu3 = CreateTestTransUnit("What.test", "What is good test.", new string[] {}, true);
+			var tu3 = CreateTestTransUnit("What.test", "What is good test.", new string[] {"Whatever you say..."}, true);
 			doc.AddTransUnit(tu3);
 			var tu4 = CreateTestTransUnit("What.this", "What is this nonsense?", new string[] {}, true);
 			doc.AddTransUnit(tu4);
@@ -731,6 +731,8 @@ namespace L10NSharp.Tests
 			// Test for invalid dynamic setting
 			var tu = doc.GetTransUnitForId("What.this");
 			tu.Dynamic = false;
+			tu.Notes.Clear();
+			tu.AddNote("ID: What.this");
 
 			// Test for deleted static unit
 			tu = doc.GetTransUnitForId("That.test");
@@ -739,6 +741,8 @@ namespace L10NSharp.Tests
 			// Test for modified unit
 			tu = doc.GetTransUnitForId("What.test");
 			tu.Source.Value = "What is a good test?";
+			tu.Notes.Clear();
+			tu.AddNote("ID: What.test");
 
 			// Test for deleted (or not covered) dynamic unit
 			tu = doc.GetTransUnitForId("How.now");
