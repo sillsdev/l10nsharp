ceedf348eac83019e281fa7b5d22223b98cf806b
Stephen McConnel <stephen_mcconnel@sil.org>
2018-01-09 16:18:13 -0700

    Add CheckOrFixXliff program (BL-5409, BL-5415)

---
+++ src/CheckOrFixXliff/Program.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.XPath;
+using L10NSharp;
+using L10NSharp.XLiffUtils;
+
+namespace CheckOrFixXliff
+{
+	/// <summary>
+	/// This program optionally validates XLiff files in three ways:
+	/// 1) Check for being well-formed XML.  This is the most basic check, and must pass for anything else to work.
+	/// 2) Check against the XLiff 1.2 schema.  This is optional ("--validate" flag), and not really needed for Bloom
+	///    and other L10NSharp clients who ignore the exact order of the child elements in trans-unit elements.  Note
+	///    that crowdin doesn't always produce valid Xliff, and they don't seem all that concerned about fixing it.
+	/// 3) Check all translated format strings (those that contain markers like '{0}' for validity: markers match
+	///    between the source and target strings, and the markers in the target strings are not mangled in translation.
+	///    If malformed markers are detected that would cause the program to crash, the 3 characters preceding and 6
+	///    characters following each open brace ('{') are displayed following the warning messages for the string.
+	/// If the third check reveals problems involving mangled substitution markers (most often in RTL scripts), the
+	/// program can optionally try to repair the strings using some common patterns that have been observed ("--fix"
+	/// flag).
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// List of trans-unit id values whose target value contains a malformed substitution marker.
+		/// </summary>
+		private static List<string> _mangledTargets = new List<string>();
+		private static bool _quiet = false;
+
+		static void Main(string[] args)
+		{
+			bool validate = false;
+			bool fix = false;
+			for (int i = 0; i < args.Length; ++i)
+			{
+				if (args[i] == "--validate")
+				{
+					validate = true;
+				}
+				else if (args[i] == "--fix")
+				{
+					fix = true;
+				}
+				else if (args[i] == "--quiet")
+				{
+					_quiet = true;
+				}
+				else
+				{
+					string filename = args[i];
+					if (!File.Exists(filename))
+					{
+						Console.WriteLine("{0} does not exist!", filename);
+					}
+					if (!CheckForWellFormedXml(filename))
+						return;	// all other checks depend on loading XML
+					if (validate)
+						ValidateXliffAgainstSchema(filename);
+					CheckFormatStringMarkers(filename);
+					if (fix)
+						FixMangledFormatMarkers(filename);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Try to fix any target strings in the file that have one or more mangled substitution markers.
+		/// </summary>
+		/// <remarks>
+		/// XDocument is used to preserve element ordering and other aspects of the xliff file that may be
+		/// modified or omitted by XliffDocument.
+		/// </remarks>
+		private static void FixMangledFormatMarkers(string filename)
+		{
+			if (_mangledTargets.Count == 0)
+				return;
+			var reader = XmlReader.Create(new StreamReader(filename));
+			var document = XDocument.Load(reader, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo | LoadOptions.SetBaseUri);
+			var namespaceManager = new XmlNamespaceManager(reader.NameTable);
+			namespaceManager.AddNamespace("x", "urn:oasis:names:tc:xliff:document:1.2");
+			foreach (var id in _mangledTargets)
+			{
+				var xpath = string.Format("//x:trans-unit[@id='{0}']", id);
+				var tu = document.XPathSelectElement(xpath, namespaceManager);
+				if (tu != null)
+					FixBrokenTransUnit(tu, namespaceManager);
+			}
+			document.Save(filename + "-fixed");
+		}
+
+		private static void FixBrokenTransUnit(XElement tu, XmlNamespaceManager namespaceManager)
+		{
+			var tuid = tu.Attribute("id").Value;
+			var source = tu.XPathSelectElement("x:source", namespaceManager);
+			var target = tu.XPathSelectElement("x:target", namespaceManager);
+			if (source.HasElements || target.HasElements)
+			{
+				Console.WriteLine("Cannot fix {0} because the translated material contains XML elements", tuid);
+				return;
+			}
+			var targetValue = target.Value;
+			if (!string.IsNullOrWhiteSpace(targetValue))
+			{
+				var targetFixed = LocalizedStringCache.FixBrokenFormattingString(targetValue);
+				if (targetFixed != target.Value)
+					target.SetValue(targetFixed);
+			}
+		}
+
+		/// <summary>
+		/// Check whether the given file is even valid ("well formed") XML.
+		/// </summary>
+		/// <returns>
+		/// true if okay, false if the file cannot load as an XmlDocument
+		/// </returns>
+		private static bool CheckForWellFormedXml(string filename)
+		{
+			try
+			{
+				var xdoc = new XmlDocument();
+				xdoc.Load(filename);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Check whether the given file validates against the XLIFF 1.2 schema.
+		/// </summary>
+		/// <returns>
+		/// true if okay, false if the file fails to validate
+		/// </returns>
+		/// <remarks>
+		/// TODO: tweak (break) our copy of the schema to disregard the order of child elements of trans-unit.
+		/// </remarks>
+		private static bool ValidateXliffAgainstSchema(string filename)
+		{
+			bool valid = true;
+			var installedXliffDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var schemaLocation = Path.Combine(installedXliffDir, "xliff-core-1.2-transitional.xsd");
+			var schemas = new XmlSchemaSet();
+			using (var reader = XmlReader.Create(schemaLocation))
+			{
+				schemas.Add("urn:oasis:names:tc:xliff:document:1.2", reader);
+				var document = XDocument.Load(filename, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo | LoadOptions.SetBaseUri);
+				document.Validate(schemas, (sender, args) => {
+					if (!_quiet)
+						Console.WriteLine("{0} did not validate against schema on line {1}: {2}", filename, args.Exception.LineNumber, args.Message);
+					valid = false;
+				});
+			}
+			return valid;
+		}
+
+		/// <summary>
+		/// Check that all the format substitution markers are present and okay.
+		/// </summary>
+		/// <returns>
+		/// true if the translation substitution markers are all valid and match the source markers, false otherwise
+		/// </returns>
+		private static bool CheckFormatStringMarkers(string filename)
+		{
+			bool allOkay = true;
+			var doc = XLiffDocument.Read(filename);
+			var dictSourceMarkers = new Dictionary<string, int>();
+			var dictTargetMarkers = new Dictionary<string, int>();
+			foreach (var tu in doc.File.Body.TransUnits)
+			{
+				if (tu.Source == null || tu.Target == null)
+					continue;
+				if (String.IsNullOrWhiteSpace(tu.Source.Value) || String.IsNullOrWhiteSpace(tu.Target.Value))
+					continue;
+				var matchesSource = Regex.Matches(tu.Source.Value, "{[0-9]+}");
+				var matchesTarget = Regex.Matches(tu.Target.Value, "{[0-9]+}");
+				if (matchesSource.Count == 0 && matchesTarget.Count == 0)
+					continue;
+				TabulateMarkers(matchesSource, dictSourceMarkers);
+				TabulateMarkers(matchesTarget, dictTargetMarkers);
+				var okay = CheckForExactlyMatchingSubstitutionMarkers(tu.Id, dictSourceMarkers, dictTargetMarkers);
+				if (!LocalizedStringCache.CheckForValidSubstitutionMarkers(dictSourceMarkers.Count, tu.Target.Value, tu.Id, _quiet))
+				{
+					_mangledTargets.Add(tu.Id);
+					okay = false;
+				}
+				if (!okay && !_quiet)
+					Console.WriteLine();	// separate the messages for differect trans-units
+				allOkay &= okay;
+			}
+			return allOkay;
+		}
+
+		/// <summary>
+		/// Check that the target substitution markers exactly match those in the source.
+		/// </summary>
+		/// <returns>
+		/// true if everything matches, false if there's any disparity
+		/// </returns>
+		private static bool CheckForExactlyMatchingSubstitutionMarkers(string tuId, Dictionary<string, int> dictSourceMarkers, Dictionary<string, int> dictTargetMarkers)
+		{
+			bool retval = true;
+			foreach (var key in dictSourceMarkers.Keys)
+			{
+				if (dictTargetMarkers.ContainsKey(key))
+				{
+					if (dictTargetMarkers[key] < dictSourceMarkers[key])
+					{
+						// missing instances, but may be valid
+						if (!_quiet)
+							Console.WriteLine(@"Translation of {0} is missing {1} copies of the marker: {2}",
+								tuId, dictSourceMarkers[key] - dictTargetMarkers[key], key);
+						retval = false;
+					}
+					else if (dictTargetMarkers[key] > dictSourceMarkers[key])
+					{
+						// extra instances, but may be valid
+						if (!_quiet)
+							Console.WriteLine(@"Translation of {0} has {1} extra copies of the marker: {2}",
+								tuId, dictTargetMarkers[key] - dictSourceMarkers[key], key);
+						retval = false;
+					}
+				}
+				else
+				{
+					// missing altogether, probably invalid
+					if (!_quiet)
+						Console.WriteLine(@"Translation of {0} is missing the marker: {1}", tuId, key);
+					retval = false;
+				}
+			}
+			foreach (var key in dictTargetMarkers.Keys)
+			{
+				if (!dictSourceMarkers.ContainsKey(key))
+				{
+					// introduced instance, certainly invalid!
+					if (!_quiet)
+						Console.WriteLine(@"Translation of {0} has an unexpected marker: {1}", tuId, key);
+					retval = false;
+				}
+			}
+			return retval;
+		}
+
+		/// <summary>
+		/// Store the different markers found by the regex search and how often each occurs in the provided dictionary.
+		/// </summary>
+		private static void TabulateMarkers(MatchCollection matches, Dictionary<string, int> dictMarkers)
+		{
+			dictMarkers.Clear();
+			for (int i = 0; i < matches.Count; ++i)
+			{
+				var key = matches[i].Value;
+				if (dictMarkers.ContainsKey(key))
+				{
+					var val = dictMarkers[key];
+					dictMarkers[key] = val + 1;
+				}
+				else
+				{
+					dictMarkers.Add(key, 1);
+				}
+			}
+		}
+	}
+}
---
+++ src/L10NSharpTests/LocalizedStringCacheTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace L10NSharp.Tests
+{
+	[TestFixture]
+	public class LocalizedStringCacheTests
+	{
+		[Test]
+		[TestCase(0, "This is a test.", true, TestName="CheckSubstitutionMarkers_1")]
+		[TestCase(1, "This is a {0}.", true, TestName="CheckSubstitutionMarkers_2")]
+		[TestCase(2, "This is {0} for {1}.", true, TestName="CheckSubstitutionMarkers_3")]
+		[TestCase(3, "This is a {0} test of {1} for {2}.", true, TestName="CheckSubstitutionMarkers_4")]
+		[TestCase(4, "This is a {3} test {2} for {1} because {0}.", true, TestName="CheckSubstitutionMarkers_5")]
+
+		[TestCase(1, "This is a {0{.", false, TestName="CheckSubstitutionMarkers_6")]
+		[TestCase(2, "This is {0 for 1}.", false, TestName="CheckSubstitutionMarkers_7")]
+		[TestCase(3, "This is a }0} test of { 1 } for {2}.", false, TestName="CheckSubstitutionMarkers_8")]
+		[TestCase(4, "This is a {3} test {} for {1} because {0}.", false, TestName="CheckSubstitutionMarkers_9")]
+
+		[TestCase(1, "\u0645 '{\u200E'{0 \u0627", false, TestName="CheckSubstitutionMarkers_10")]
+		[TestCase(1, "\u0647 '{\u200E'{0\u0646\u0627", false, TestName="CheckSubstitutionMarkers_11")]
+		[TestCase(1, "\u0632 \"{\u200E\"{0\u200F \u0627", false, TestName="CheckSubstitutionMarkers_12")]
+		[TestCase(1, "\u0627\u06CC {\u200E0}\u200F \u0627", false, TestName="CheckSubstitutionMarkers_13")]
+		[TestCase(1, "\u0627\u06CC {\u200E{0", false, TestName="CheckSubstitutionMarkers_14")]
+		[TestCase(1, "\u0647 '{\u200E0}'\u200F \u0627", false, TestName="CheckSubstitutionMarkers_15")]
+		[TestCase(1, "\u062A\u0646 {\u200E {0 \u0631", false, TestName="CheckSubstitutionMarkers_16")]
+		[TestCase(1, "\u0632\u0020\"{\u200E\"{0\u200F.", false, TestName="CheckSubstitutionMarkers_17")]
+		public void CheckStringsForValidSubstitionMarkers(int markerCount, string formatting, bool isValid)
+		{
+			Assert.That(LocalizedStringCache.CheckForValidSubstitutionMarkers(markerCount, formatting, "a.b"), Is.EqualTo(isValid));
+		}
+
+		[Test]
+		[TestCase("\u0645 '{\u200E'{0 \u0627",           "\u0645 \u200E'{0}'\u200F \u0627",      TestName = "FixBrokenFormattingString_Works_1")]
+		[TestCase("\u0647 '{\u200E'{0\u0646\u0627",      "\u0647 \u200E'{0}'\u200F\u0646\u0627", TestName = "FixBrokenFormattingString_Works_2")]
+		[TestCase("\u0632 \"{\u200E\"{0\u200F \u0627",   "\u0632 \u200E\"{0}\"\u200F \u0627",    TestName = "FixBrokenFormattingString_Works_3")]
+		[TestCase("\u0627\u06CC {\u200E0}\u200F \u0627", "\u0627\u06CC \u200E{0}\u200F \u0627",  TestName = "FixBrokenFormattingString_Works_4")]
+		[TestCase("\u0627\u06CC {\u200E{0",              "\u0627\u06CC \u200E{0}\u200F",         TestName = "FixBrokenFormattingString_Works_5")]
+		[TestCase("\u0647 '{\u200E0}'\u200F \u0627",     "\u0647 \u200E'{0}'\u200F \u0627",      TestName = "FixBrokenFormattingString_Works_6")]
+		[TestCase("\u062A\u0646 {\u200E {0 \u0631",      "\u062A\u0646 \u200E{0}\u200F  \u0631",  TestName = "FixBrokenFormattingString_Works_7")]
+		[TestCase("\u0632 \"{\u200E\"{0\u200F.",         "\u0632 \u200E\"{0}\"\u200F.",          TestName = "FixBrokenFormattingString_Works_8")]
+		public void TryToFixBrokenSubstitutionMarkers(string badFormat, string goodFormat)
+		{
+			var result = LocalizedStringCache.FixBrokenFormattingString(badFormat);
+			Assert.That(result, Is.EqualTo(goodFormat));
+			Assert.That(LocalizedStringCache.CheckForValidSubstitutionMarkers(1, result, "a.b"), Is.True);
+		}
+	}
+}
