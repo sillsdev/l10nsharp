590ca1a8d0965e8f312d6d8b2e92bbb343098038
Eberhard Beilharz <eb1@sil.org>
2019-02-12 16:22:23 +0100

    Strong-name assembly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,3 +19,4 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ### Changed
 
 - Create nuget package
+- Strong-name assembly
--- src/L10NSharp/Properties/AssemblyInfo.cs
+++ src/L10NSharp/Properties/AssemblyInfo.cs
@@ -1,5 +1,12 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("L10NSharpTests")]
-[assembly: InternalsVisibleTo("ExtractXliff")]
-[assembly: InternalsVisibleTo("CheckOrFixXliff")]
+// to get the public key, run:
+// 1. extract the public key from the keyfile with
+// sn –p <keyfile.snk> <keyfile.snk.PublicKey>
+// 2. show the public key with
+// sn -tp <keyfile.snk.PublicKey>
+// (use the full public key, not the public key token!)
+
+[assembly: InternalsVisibleTo("L10NSharpTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010019cae48c6e5703818db31fcca17b87012eef38df95e3961c59b035190eace2c4ee5cfa1a258b84867c7549f60eec00ec1e0004c2ed224457128e841311cea0f5a05c3d69b3dfcb7422b214febce6e06c83ce4d29c62f36a7fd5564e922338c800372a5ec09638671b4db1fb33ccbb1dc48d8122ffe0d30dadbfbf325f65437b2")]
+[assembly: InternalsVisibleTo("ExtractXliff" PublicKey=002400000480000094000000060200000024000052534131000400000100010019cae48c6e5703818db31fcca17b87012eef38df95e3961c59b035190eace2c4ee5cfa1a258b84867c7549f60eec00ec1e0004c2ed224457128e841311cea0f5a05c3d69b3dfcb7422b214febce6e06c83ce4d29c62f36a7fd5564e922338c800372a5ec09638671b4db1fb33ccbb1dc48d8122ffe0d30dadbfbf325f65437b2")]
+[assembly: InternalsVisibleTo("CheckOrFixXliff" PublicKey=002400000480000094000000060200000024000052534131000400000100010019cae48c6e5703818db31fcca17b87012eef38df95e3961c59b035190eace2c4ee5cfa1a258b84867c7549f60eec00ec1e0004c2ed224457128e841311cea0f5a05c3d69b3dfcb7422b214febce6e06c83ce4d29c62f36a7fd5564e922338c800372a5ec09638671b4db1fb33ccbb1dc48d8122ffe0d30dadbfbf325f65437b2")]
