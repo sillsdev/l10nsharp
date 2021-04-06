1c30a9f4348c3c06db06c5a6c8b17ea5e4ed1ab1
andrew-polk <andrew_polk@sil.org>
2021-04-06 14:22:24 -0700

    Improve error handling
    Don't cause a crash by generating a zero-length file.
    Provide a mechanism by which the developer can get the root cause of extraction errors.

--- src/L10NSharp/UI/InitializationProgressDlg.cs
+++ src/L10NSharp/UI/InitializationProgressDlg.cs
@@ -34,16 +34,38 @@ namespace L10NSharp.UI
 
 		protected override void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
 		{
-			try
+			if (e.Error != null)
 			{
-				ExtractedInfo = (IEnumerable<LocalizingInfo>) e.Result;
+				var message = $"Error in extracting localizable strings: {e.Error.Message} ({e.Error})";
+				Console.WriteLine(message);
+
+				// Adding the error to the ExtractedInfo here serves two purposes.
+				// 1. It makes sure we get a valid file. Otherwise we get failures later.
+				// 2. It provides a way for the developer to see the actual error which caused extraction to fail.
+				ExtractedInfo = new[]
+				{
+					new LocalizingInfo("StringExtractor_Error")
+					{
+						LangId = "en",
+						Text = "An error occurred while collecting strings or there were no strings to collect. " +
+						       "Check comment for exception. Note, the exception may not occur again until you delete this file.",
+						Comment = message
+					}
+				};
 			}
-			catch (Exception ex)
+			else
 			{
-				Debug.WriteLine("Error in extracting localizable strings: {0} ({1})", ex.Message, e.Error);
+				try
+				{
+					ExtractedInfo = (IEnumerable<LocalizingInfo>)e.Result;
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine($"Error in extracting localizable strings: {ex.Message} ({e.Error})");
+				}
 			}
+
 			Close();
 		}
-
 	}
 }
