688042632d33cf9071b0b8666b5816647053343c
Andrew Polk <andrew_polk@sil.org>
2024-04-16 11:26:11 -0700

    Throw if the library is used before being initialized (BL-13266)
    +semver: major

--- src/L10NSharp/LocalizationManager.cs
+++ src/L10NSharp/LocalizationManager.cs
@@ -839,5 +839,14 @@ namespace L10NSharp
 		/// the localization managers.
 		/// </summary>
 		public static bool ThrowIfManagerDisposed = true;
+
+		/// <summary>
+		/// True (default) to throw if we try to get a localized string before creating any localization managers.
+		/// This is to prevent an invalid state where language IDs get mapped incorrectly at the beginning and
+		/// then never get updated which can cause us to fail to return properly localized strings when requested (see BL-13245).
+		/// The fix is to ensure that a LocalizationManager is created before calling any localization methods.
+		/// Or, to maintain prior behavior, set this to false.
+		/// </summary>
+		public static bool StrictInitializationMode = true;
 	}
 }
