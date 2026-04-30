e88cbd03fb5dcf294be2fe20c3f101f6c0252da8
tombogle <tom_bogle@sil.org>
2026-04-29 22:40:17 -0400

    Fixed platform targeting issues so that the L10nSharp project and its test project t do not build for net8.0-windows
    Fixed timing-related unit test error caused by running same test in parallel for different target frameworks
    Also pruned LocalizationManagerRunner, which has not been built or maintained for over a decade.

--- src/L10NSharp.Windows.Forms.Tests/ILocalizableComponentTests.cs
+++ src/L10NSharp.Windows.Forms.Tests/ILocalizableComponentTests.cs
@@ -26,10 +26,12 @@ namespace L10NSharp.Windows.Forms.Tests
 		/// ------------------------------------------------------------------------------------
 		protected void TestSetup(string installedTranslationDir)
 		{
+			var uniqueId = $"LocalizableComponentTest_{Guid.NewGuid()}";
+			
 			var dir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath);
 			m_manager = LocalizationManagerWinforms.Create("en", "LocalizableComponentTest", "LocalizableComponentTest", "1.0",
 					Path.Combine(dir, installedTranslationDir),
-					"", null, new string[] {  })
+					uniqueId, null, new string[] {  })
 				as ILocalizationManagerInternalWinforms<XLiffDocument>;
 			m_translationPath = m_manager.GetPathForLanguage("en", true);
 			m_extender = new L10NSharpExtender { LocalizationManagerId = "LocalizableComponentTest" };
